392c1fa96aabf653a4ef0e1fb79696f1bfd3075d
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-07-31 13:11:01 -0700

    Add install class

---
+++ modules/vscode/manifests/install.pp
@@ -0,0 +1,22 @@
+# @summary Installs Visual Studio Code.
+#
+# Downloads the Visual Studio Code system installer and then installs.
+#
+# @example
+#   include vscode::install
+class vscode::install {
+  file { 'C:\\Windows\\temp\\VSCodeSetup-x64-1.58.2.exe':
+    ensure => 'file',
+    source => 'https://az764295.vo.msecnd.net/stable/c3f126316369cd610563c75b1b1725e0679adfb3/VSCodeSetup-x64-1.58.2.exe'
+  }
+
+  package { 'Microsoft Visual Studio Code':
+    ensure          => 'present',
+    source          => 'C:\\Windows\\temp\\VSCodeSetup-x64-1.58.2.exe',
+    install_options => [
+      '/verysilent',
+      'mergetasks=!runCode,!desktopicon,!quicklaunchicon,addcontextmenufiles,addcontextmenufolders,addtopath',
+    ],
+    require         => File['C:\\Windows\\temp\\VSCodeSetup-x64-1.58.2.exe'],
+  }
+}
